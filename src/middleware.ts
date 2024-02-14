fd0c6b2c36a19f12a707758ab642ba79b0ba9cb6
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-02-14 09:54:32 +0000

    Alteracao das permissoes dos utilizadores mediante a role de cada um

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,8 +7,40 @@ export default withAuth(
     console.log(request.nextauth.token);
 
     //caso o url seja "/ordemFabrico" e o utilizador NAO seja admin não tera permissoes para prosseguir
+    /*
+    if (
+      request.nextUrl.pathname.match("/") &&
+      request.nextauth.token?.role !== "admin"
+    ) {
+      return NextResponse.rewrite(new URL("/denied", request.url));
+    }
+    */
+    if (
+      request.nextUrl.pathname.startsWith("/dashboard") &&
+      request.nextauth.token?.role !== "admin" &&
+      request.nextauth.token?.role !== "dashboard"
+    ) {
+      return NextResponse.rewrite(new URL("/denied", request.url));
+    }
+
     if (
       request.nextUrl.pathname.startsWith("/ordemFabrico") &&
+      request.nextauth.token?.role !== "admin" &&
+      request.nextauth.token?.role !== "ordensfabrico"
+    ) {
+      return NextResponse.rewrite(new URL("/denied", request.url));
+    }
+
+    if (
+      request.nextUrl.pathname.startsWith("/producao") &&
+      request.nextauth.token?.role !== "admin" &&
+      request.nextauth.token?.role !== "producao"
+    ) {
+      return NextResponse.rewrite(new URL("/denied", request.url));
+    }
+
+    if (
+      request.nextUrl.pathname.startsWith("/opcoes") &&
       request.nextauth.token?.role !== "admin"
     ) {
       return NextResponse.rewrite(new URL("/denied", request.url));
@@ -21,4 +53,6 @@ export default withAuth(
   }
 );
 
-export const config = { matcher: ["/ordemFabrico"] };
+export const config = {
+  matcher: ["/", "/dashboard", "/ordemFabrico", "/producao", "/opcoes"],
+};
