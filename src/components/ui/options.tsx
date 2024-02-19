fd41cddf0ff39089394a8cccecc09b810b881b30
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-02-19 17:32:10 +0000

    criar visibilidade nas opcoes do campo que esta a ser utilizado

--- src/components/ui/options.tsx
+++ src/components/ui/options.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { link } from "fs";
 import {
   LayoutDashboard,
   ClipboardList,
@@ -6,12 +8,44 @@ import {
   Warehouse,
   Boxes,
 } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export default function Options() {
+  const currentPathname = usePathname();
+
+  const links = [
+    {
+      label: "dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "chegadaMP",
+      href: "/chegadaMateriaPrima",
+    },
+    {
+      label: "ordemFabrico",
+      href: "/ordemFabrico",
+    },
+    {
+      label: "producao",
+      href: "/producao",
+    },
+    {
+      label: "maisPrafrente",
+      href: "/maisPrafrente",
+    },
+  ];
+
   return (
     <div className="grid grid-flow-col auto-cols-max mt-10 justify-evenly gap-x-4">
-      <a href="/dashboard">
-        <Card className="flex flex-col w-[190px] border-lime-500 items-center justify-center pt-5 drop-shadow-2xl hover:bg-gradient-to-br from-white to-lime-500 to-80% duration-275 ease-in  hover:translate-y-1 hover:translate-x-1 hover:border-0">
+      <a href="dashboard">
+        <Card
+          className={`${
+            links[0].href.startsWith(currentPathname.toString())
+              ? "bg-gradient-to-br from-white to-lime-500 border-none"
+              : "bg-white"
+          } flex flex-col w-[190px] border-lime-500 items-center justify-center pt-5 drop-shadow-2xl hover:bg-gradient-to-br from-white to-lime-500 to-80% duration-275 ease-in  hover:translate-y-1 hover:translate-x-1 hover:border-0`}
+        >
           <CardContent>
             <LayoutDashboard />
           </CardContent>
@@ -20,8 +54,14 @@ export default function Options() {
           </CardFooter>
         </Card>
       </a>
-      <a href="/chegadaMateriaPrima">
-        <Card className="flex flex-col w-[190px] border-lime-500 items-center justify-center pt-5 drop-shadow-2xl hover:bg-gradient-to-br from-white to-lime-500 to-80% duration-275 ease-in  hover:translate-y-1 hover:translate-x-1 hover:border-0">
+      <a href="chegadaMateriaPrima">
+        <Card
+          className={`${
+            links[1].href.startsWith(currentPathname.toString())
+              ? "bg-gradient-to-br from-white to-lime-500 border-none"
+              : "bg-white"
+          } flex flex-col w-[190px] border-lime-500 items-center justify-center pt-5 drop-shadow-2xl hover:bg-gradient-to-br from-white to-lime-500 to-80% duration-275 ease-in  hover:translate-y-1 hover:translate-x-1 hover:border-0`}
+        >
           <CardContent>
             <Boxes />
           </CardContent>
@@ -31,7 +71,13 @@ export default function Options() {
         </Card>
       </a>
       <a href="ordemFabrico">
-        <Card className="flex flex-col w-[190px] border-lime-500 items-center justify-center pt-5 drop-shadow-2xl hover:bg-gradient-to-br from-white to-lime-500 to-80% duration-275 ease-in hover:translate-y-1 hover:translate-x-1 hover:border-0">
+        <Card
+          className={`${
+            links[2].href.startsWith(currentPathname.toString())
+              ? "bg-gradient-to-br from-white to-lime-500 border-none"
+              : "bg-white"
+          } flex flex-col w-[190px] border-lime-500 items-center justify-center pt-5 drop-shadow-2xl hover:bg-gradient-to-br from-white to-lime-500 to-80% duration-275 ease-in  hover:translate-y-1 hover:translate-x-1 hover:border-0`}
+        >
           <CardContent>
             <ClipboardList />
           </CardContent>
@@ -41,7 +87,13 @@ export default function Options() {
         </Card>
       </a>
       <a href="producao">
-        <Card className="flex flex-col w-[190px] border-lime-500 items-center justify-center pt-5 drop-shadow-2xl hover:bg-gradient-to-br from-white to-lime-500 to-80% duration-275 ease-in hover:translate-y-1 hover:translate-x-1 hover:border-0">
+        <Card
+          className={`${
+            links[3].href.startsWith(currentPathname.toString())
+              ? "bg-gradient-to-br from-white to-lime-500 border-none"
+              : "bg-white"
+          } flex flex-col w-[190px] border-lime-500 items-center justify-center pt-5 drop-shadow-2xl hover:bg-gradient-to-br from-white to-lime-500 to-80% duration-275 ease-in  hover:translate-y-1 hover:translate-x-1 hover:border-0`}
+        >
           <CardContent>
             <Factory />
           </CardContent>
@@ -51,7 +103,13 @@ export default function Options() {
         </Card>
       </a>
       <a href="maisPrafrente">
-        <Card className="flex flex-col w-[190px] border-lime-500 items-center justify-center pt-5 drop-shadow-2xl hover:bg-gradient-to-br from-white to-lime-500 to-80% duration-275 ease-in hover:translate-y-1 hover:translate-x-1 hover:border-0">
+        <Card
+          className={`${
+            links[4].href.startsWith(currentPathname.toString())
+              ? "bg-gradient-to-br from-white to-lime-500 border-none"
+              : "bg-white"
+          } flex flex-col w-[190px] border-lime-500 items-center justify-center pt-5 drop-shadow-2xl hover:bg-gradient-to-br from-white to-lime-500 to-80% duration-275 ease-in  hover:translate-y-1 hover:translate-x-1 hover:border-0`}
+        >
           <CardContent>
             <Warehouse />
           </CardContent>
