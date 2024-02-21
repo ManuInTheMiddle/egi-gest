c10bf3a1065d8beda42dd9d3241ee83db6019b60
emRibeiroJPM <emanuel.ribeiro@jpm.pt>
2024-02-21 23:01:07 +0000

    update da localizaçao do globals css

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+//import "./globals.css";
+import "../globals.css";
 import Navbar from "@/components/ui/navbar";
 import Footer from "@/components/ui/footer";
 import Options from "@/components/ui/options";
