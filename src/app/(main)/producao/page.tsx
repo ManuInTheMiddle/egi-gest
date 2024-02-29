7dbc0dcb497f7f7f058a9f8f6188975ebcf5c759
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-02-29 11:36:20 +0000

    adicao da tabela a area de producao

--- src/app/(main)/producao/page.tsx
+++ src/app/(main)/producao/page.tsx
@@ -1,8 +1,22 @@
+import DataTable from "./data-table";
+import { ordensProducao } from "../../../../public/assets/mockData/ordensFabrico";
+import ColumnsComponent from "./columns";
+
+/*
+const getData = async () => {
+  //fetch das ordens de fabrico provindas do SAP
+  return ordensProducao;
+};
+*/
+
 const page = () => {
+  const dados = ordensProducao;
+  const semDados = [];
   return (
     <div className="m-4 mt-10">
       <section className="border-2 border-slate-600 rounded-md p-7 shadow-2xl">
         <h1 className="text-lg mb-3">Produção</h1>
+        <DataTable columns={ColumnsComponent} data={dados} />
       </section>
     </div>
   );
