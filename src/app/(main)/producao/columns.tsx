ffb79e86c1858d5858f0e47b16c7368e373c0e6e
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-02-29 16:01:17 +0000

    adition of actions into the row

--- src/app/(main)/producao/columns.tsx
+++ src/app/(main)/producao/columns.tsx
@@ -1,6 +1,17 @@
 "use client";
 import { ordemProducao } from "../../../../public/assets/mockData/ordensFabrico";
 import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const ColumnsComponent: ColumnDef<ordemProducao>[] = [
   { accessorKey: "referencia", header: "Referência" },
@@ -8,6 +19,34 @@ const ColumnsComponent: ColumnDef<ordemProducao>[] = [
   { accessorKey: "descricao", header: "Descrição" },
   { accessorKey: "quantidade", header: "Quantidade" },
   { accessorKey: "status", header: "Status" },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const ordemProd = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Abrir Menu</span>
+              <MoreHorizontal />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Opções</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() =>
+                navigator.clipboard.writeText(ordemProd.loteFabrico)
+              }
+            >
+              Copiar Lote Fabrico
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Detalhes da Ordem</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
 ];
 
 export default ColumnsComponent;
