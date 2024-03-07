ff9f7706e329323307dae60e27217747e0607cfc
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-07 16:43:18 +0000

    get all production orders

--- src/app/api/ordensProducao/route.ts
+++ src/app/api/ordensProducao/route.ts
@@ -2,7 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../prisma/db";
 export const GET = async (req: NextRequest) => {
   try {
-    const ordensProducao = await prisma.ordens_producao.findMany({});
+    const ordensProducao = await prisma.ordens_producao.findMany({
+      select: {
+        id_ordem_producao: true,
+        status_ordens_producao: { select: { name: true } },
+        lote_fabrico: true,
+        quantidade: true,
+        data_criacao: true,
+        produtos: true,
+        detalhes_ordens_producao: true,
+      },
+    });
+
     return NextResponse.json(
       { message: "OK", ordensProducao },
       { status: 200 }
