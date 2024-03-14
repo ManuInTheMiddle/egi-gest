20eaf420f885f7bb7c7a41cd284ec78cd922e83d
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-14 12:15:06 +0000

    added tanstack query to layout

---
+++ src/app/(producao)/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import TanstackProvider from "../../../providers/TanstackProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "MESlette",
+  description: "Controlador de paletes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="no-scrollbar">
+      <body className={inter.className}>
+        <TanstackProvider>
+          <section>{children}</section>
+        </TanstackProvider>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
