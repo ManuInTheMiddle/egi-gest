732f9ae2f27f995841aa324d1977c75afc3ed21c
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-18 16:33:44 +0000

    states for the ordem de producao

---
+++ src/app/(producao)/componentes/estados.ts
@@ -0,0 +1,6 @@
+export const estadosOrdem = [
+  { id: 1, value: "1", estado: "Planeada" },
+  { id: 2, value: "2", estado: "A Decorrer" },
+  { id: 3, value: "3", estado: "Finalizada" },
+  { id: 4, value: "4", estado: "Cancelada" },
+];
