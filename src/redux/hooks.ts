e9f5318d1b4ffc4e4b7499ea86dab5078f3b2c5d
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-18 10:48:47 +0000

    creation pre typed hooks for redux toolkit

---
+++ src/redux/hooks.ts
@@ -0,0 +1,8 @@
+import { UseDispatch, useSelector, useStore } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+import type { RootState, AppDispatch, AppStore } from "./store";
+import { useDispatch } from "react-redux";
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppStore: () => AppStore = useStore;
