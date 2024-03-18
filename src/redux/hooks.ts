e9c673210867fc2533c4bf68e20aa292860cc5c6
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-18 11:45:30 +0000

    typo corrections

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,7 +1,6 @@
-import { UseDispatch, useSelector, useStore } from "react-redux";
+import { useDispatch, useSelector, useStore } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
 import type { RootState, AppDispatch, AppStore } from "./store";
-import { useDispatch } from "react-redux";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
