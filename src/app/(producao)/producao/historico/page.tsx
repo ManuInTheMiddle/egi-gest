1eb1fb27965c1813002a14d47786ec5129f9414a
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-19 15:33:58 +0000

    adjust height of the black border to acomodate all components inside(TRUE ONE)

--- src/app/(producao)/producao/historico/page.tsx
+++ src/app/(producao)/producao/historico/page.tsx
@@ -5,7 +5,7 @@ import RenderizarTable from "./RenderizarTable";
 const Historico = () => {
   return (
     <div className="flex flex-col">
-      <section className="border-2 border-slate-600 rounded-md p-5 shadow-2xl items-center min-h-full justify-start h-max m-1">
+      <section className="border-2 border-slate-600 rounded-md p-5 shadow-2xl items-center min-h-full justify-start h- m-1">
         <Retorceder />
         <RenderizarTable />
       </section>
