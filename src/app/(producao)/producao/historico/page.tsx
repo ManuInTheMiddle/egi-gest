4aae0a58996278a7781bbf59f793a6f8fc13a54b
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-19 15:38:23 +0000

    fixed some height problems

--- src/app/(producao)/producao/historico/page.tsx
+++ src/app/(producao)/producao/historico/page.tsx
@@ -5,7 +5,7 @@ import RenderizarTable from "./RenderizarTable";
 const Historico = () => {
   return (
     <div className="flex flex-col">
-      <section className="border-2 border-slate-600 rounded-md p-5 shadow-2xl items-center min-h-full justify-start h- m-1">
+      <section className="border-2 border-slate-600 rounded-md p-5 shadow-2xl items-center min-h-screen justify-start h-full m-1">
         <Retorceder />
         <RenderizarTable />
       </section>
