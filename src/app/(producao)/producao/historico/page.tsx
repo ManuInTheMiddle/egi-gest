659e02f90cfa0c5e2198e46e5740e3df02b956f9
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-19 09:43:38 +0000

    added table and the element that allows to return to the last page (retorceder)

---
+++ src/app/(producao)/producao/historico/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Retorceder from "./componentes/retorceder";
+import RenderizarTable from "./RenderizarTable";
+
+const Historico = () => {
+  return (
+    <div className="flex flex-col">
+      <section className="border-2 border-slate-600 rounded-md p-5 shadow-2xl items-center min-h-full justify-start h-screen m-1">
+        <Retorceder />
+        <RenderizarTable />
+      </section>
+    </div>
+  );
+};
+
+export default Historico;
