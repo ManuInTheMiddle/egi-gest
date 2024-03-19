41500a73cda0de16a11b7f2a5e0fa780b4d1aa9b
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-19 12:02:29 +0000

    added pagination to transaction

--- src/app/api/ordensProducao/historico/route.ts
+++ src/app/api/ordensProducao/historico/route.ts
@@ -24,3 +24,35 @@ export const POST = async (req: NextRequest) => {
     console.error(error);
   }
 };
+
+export const GET = async (req: NextRequest) => {
+  const skipPageNum = req.nextUrl.searchParams.get("skip");
+  try {
+    const historicoOrdens = skipPageNum
+      ? await prisma.transaction_ordens_producao.findMany({
+          skip: parseInt(skipPageNum),
+          take: 10,
+          select: {
+            ordens_producao: true,
+            status_ordens_producao_transaction_ordens_producao_status_anteriorTostatus_ordens_producao:
+              true,
+            status_ordens_producao_transaction_ordens_producao_status_novoTostatus_ordens_producao:
+              true,
+            timestamp: true,
+          },
+        })
+      : await prisma.transaction_ordens_producao.findMany({
+          select: {
+            ordens_producao: true,
+            status_ordens_producao_transaction_ordens_producao_status_anteriorTostatus_ordens_producao:
+              true,
+            status_ordens_producao_transaction_ordens_producao_status_novoTostatus_ordens_producao:
+              true,
+            timestamp: true,
+          },
+        });
+    return NextResponse.json({ message: "OK", historicoOrdens });
+  } catch (error) {
+    console.error(error);
+  }
+};
