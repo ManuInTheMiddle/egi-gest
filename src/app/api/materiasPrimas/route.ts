cfccb5f9fa21a57aca9adb22d7a4d5bc0c17f7b6
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-19 12:00:51 +0000

    added pagination to materias primas

--- src/app/api/materiasPrimas/route.ts
+++ src/app/api/materiasPrimas/route.ts
@@ -1,8 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+
 import prisma from "../../../../prisma/db";
-export const GET = async () => {
+
+export const GET = async (req: NextRequest) => {
+  const skipPageNum = req.nextUrl.searchParams.get("skip");
   try {
-    const materiasPrimas = await prisma.materias_primas.findMany({});
+    const materiasPrimas = skipPageNum
+      ? await prisma.materias_primas.findMany({
+          skip: parseInt(skipPageNum!),
+          take: 10,
+        })
+      : await prisma.materias_primas.findMany({});
 
     return NextResponse.json(
       { mensagem: "OK", materiasPrimas },
