7276e553011e9de0f16d0a725d3dce393f89516f
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-19 15:27:36 +0000

    added pagination of the orders history

--- src/app/(producao)/producao/historico/RenderizarTable.tsx
+++ src/app/(producao)/producao/historico/RenderizarTable.tsx
@@ -1,29 +1,116 @@
 "use client";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import DataTable from "../historico/componentes/data-table";
 import ColumnsComponent from "../historico/componentes/columns";
+import { ArrowRightCircle, ArrowLeftCircle } from "lucide-react";
 import axios from "axios";
 import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  TooltipProvider,
+  TooltipTrigger,
+  TooltipContent,
+  Tooltip,
+} from "@/components/ui/tooltip";
 
 const RenderizarTable = () => {
   const [data, setData] = useState([]);
+  const primeiraRenderizacao = useRef(true);
+  const [desativar, setdesativar] = useState(true);
+  const [desativarSeguinte, setDesativarSeguinte] = useState(false);
+  const pagNum = useRef(0);
+
+  const handlePaginaSeguinte = async () => {
+    pagNum.current = pagNum.current + 10;
+    pagNum.current === 0 ? setdesativar(true) : setdesativar(false);
+    //console.log(pagNum);
+    try {
+      const resposta = await axios.get(
+        `http://localhost:3000/api/ordensProducao/historico?skip=${pagNum.current}`
+      );
+      const dados = await resposta.data.historicoOrdens;
+      setData(dados);
+      if (!dados.length) {
+        setDesativarSeguinte(true);
+      }
+
+      //console.log(dados);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handlePaginaAnterior = async () => {
+    pagNum.current = pagNum.current - 10;
+    pagNum.current === 0 ? setdesativar(true) : setdesativar(false);
+    //console.log(pagNum);
+    try {
+      const resposta = await axios.get(
+        `http://localhost:3000/api/ordensProducao/historico?skip=${pagNum.current}`
+      );
+      const dados = await resposta.data.historicoOrdens;
+      setData(dados);
+      if (dados.length) {
+        setDesativarSeguinte(false);
+      }
+      //console.log(dados);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const getHistoricoOrdens = async () => {
     const resposta = await axios.get(
-      "http://localhost:3000/api/ordensProducao/historico"
+      `http://localhost:3000/api/ordensProducao/historico?skip=0`
     );
     const dados = await resposta.data.historicoOrdens;
     setData(dados);
     //console.log(dados);
+    primeiraRenderizacao.current = false;
     return dados;
   };
 
   useEffect(() => {
-    getHistoricoOrdens();
-  }, []);
+    if (primeiraRenderizacao.current) {
+      getHistoricoOrdens();
+    }
+  }, [data]);
   return (
     <>
       <DataTable columns={ColumnsComponent} data={data} />
+      <div className="flex flex-row mt-5 items-center justify-center gap-4">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                size={"icon"}
+                variant={"ghost"}
+                onClick={() => handlePaginaAnterior()}
+                disabled={desativar}
+              >
+                <ArrowLeftCircle color="#84CC27" size={30} strokeWidth={1.5} />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Pagina Anterior</p>
+            </TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                size={"icon"}
+                variant={"ghost"}
+                onClick={() => handlePaginaSeguinte()}
+                disabled={desativarSeguinte}
+              >
+                <ArrowRightCircle color="#84CC27" size={30} strokeWidth={1.5} />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Pagina Seguinte</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
     </>
   );
 };
