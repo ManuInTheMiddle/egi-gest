f42a18145a9a3ca8379385300f5ad0d9fe30ab8f
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-20 10:10:57 +0000

    added calendar filter

--- src/app/(producao)/producao/historico/componentes/columns.tsx
+++ src/app/(producao)/producao/historico/componentes/columns.tsx
@@ -1,6 +1,8 @@
-import { ColumnDef } from "@tanstack/react-table";
+import { ColumnDef, Row } from "@tanstack/react-table";
 import { Order } from "./columnsTypes";
 import { MoveRight } from "lucide-react";
+import { DateRange } from "react-day-picker";
+import { DateValues, isWithinInterval, parseISO } from "date-fns";
 
 const ColumnsComponent: ColumnDef<Order>[] = [
   {
@@ -26,7 +28,32 @@ const ColumnsComponent: ColumnDef<Order>[] = [
     header: "Estado Novo",
     id: "estadoNovo",
   },
-  { accessorKey: "timestamp", header: "Timestamp", id: "timestamp" },
+  {
+    accessorKey: "timestamp",
+    header: "Timestamp",
+    id: "timestamp",
+    filterFn: (row, columnId, filteredValue: DateRange) => {
+      const data: string = row.getValue(columnId);
+      const parsedData = parseISO(data);
+      /*
+      console.log(`Data from:${filteredValue.from}`);
+      console.log(`Data to:${filteredValue.to}`);
+      console.log(`Data da row:${parsedData}`);
+      */
+
+      const DataInicio = new Date(filteredValue.from?.toLocaleString("en-US")!);
+      const DataFim = new Date(filteredValue.to?.toLocaleString("en-US")!);
+      const DataDaRow = new Date(parsedData.toLocaleString("en-US"));
+      /*
+      console.log(
+        isWithinInterval(DataDaRow, { start: DataInicio, end: DataFim })
+      );
+      */
+      return isWithinInterval(DataDaRow, { start: DataInicio, end: DataFim })
+        ? true
+        : false;
+    },
+  },
 ];
 
 export default ColumnsComponent;
