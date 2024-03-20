bb53488789d6fead3279bb5cff1a0ac252564394
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-20 15:08:43 +0000

    added received and update reactor

---
+++ src/app/api/ordensProducao/reator/route.ts
@@ -0,0 +1,64 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../../prisma/db";
+
+interface Ireator {
+  reator: number;
+}
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const orderId = req.nextUrl.searchParams.get("order-id");
+    const reator = await prisma.ordens_producao.findMany({
+      where: { id_ordem_producao: parseInt(orderId!) },
+      select: {
+        id_ordem_producao: true,
+        reatores: true,
+        lote_fabrico: true,
+        reator: true,
+      },
+    });
+
+    if (reator.length === 0) {
+      return NextResponse.json(
+        { message: "ordem inixistente", reator },
+        { status: 406 }
+      );
+    }
+
+    return NextResponse.json({ message: "OK", reator }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Erro ao devolver ordens producao" },
+      { status: 503 }
+    );
+  }
+};
+
+export const PATCH = async (req: NextRequest) => {
+  try {
+    const orderId = req.nextUrl.searchParams.get("order-id");
+    const reatorRecebidoPeloUser: Ireator = await req.json();
+
+    const reator = await prisma.ordens_producao.findMany({
+      where: { id_ordem_producao: parseInt(orderId!) },
+      select: { id_ordem_producao: true, reatores: true, lote_fabrico: true },
+    });
+
+    if (reator.length === 0) {
+      return NextResponse.json(
+        { message: "ordem inixistente", reator },
+        { status: 406 }
+      );
+    }
+
+    await prisma.ordens_producao.update({
+      where: { id_ordem_producao: parseInt(orderId!) },
+      data: { reator: reatorRecebidoPeloUser.reator },
+    });
+    return NextResponse.json({
+      message: "OK",
+      reator,
+      reatorDefinidoPeloUser: reatorRecebidoPeloUser.reator,
+    });
+  } catch (error) {}
+};
