8bb70d300560c86f6e8d28c5b64bcfc7f697ebf2
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-20 10:25:27 +0000

    Added filter by calendar

--- src/app/(producao)/producao/historico/componentes/data-table.tsx
+++ src/app/(producao)/producao/historico/componentes/data-table.tsx
@@ -1,6 +1,10 @@
 "use client";
 import React, { useState } from "react";
 
+import { cn } from "@/lib/utils";
+import { format, subDays } from "date-fns";
+import { pt } from "date-fns/locale";
+import { DateRange } from "react-day-picker";
 import {
   ColumnFiltersState,
   getFilteredRowModel,
@@ -10,7 +14,11 @@ import {
   useReactTable,
   getPaginationRowModel,
 } from "@tanstack/react-table";
-
+import {
+  Calendar as CalendarIcon,
+  CalendarMinus2,
+  CalendarSearch,
+} from "lucide-react";
 import {
   Table,
   TableBody,
@@ -19,8 +27,20 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -32,6 +52,10 @@ export default function DataTable<TData, TValue>({
   data,
 }: DataTableProps<TData, TValue>) {
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [date, setDate] = React.useState<DateRange | undefined>({
+    from: subDays(new Date(), 30),
+    to: new Date(),
+  });
 
   const table = useReactTable({
     data,
@@ -48,29 +72,113 @@ export default function DataTable<TData, TValue>({
   return (
     <div>
       <div className="flex flex-row items-center justify-between gap-4">
-        <div className="flex items-center py-4 gap-x-2">
-          <Input
-            placeholder="Filtrar Status ... "
-            value={
-              (table.getColumn("estadoNovo")?.getFilterValue() as string) ?? ""
-            }
-            onChange={(e) =>
-              table.getColumn("estadoNovo")?.setFilterValue(e.target.value)
-            }
-            className="max-w-sm"
-          />
-          <Input
-            placeholder="Filtrar Lote ... "
-            value={
-              (table.getColumn("loteFabrico")?.getFilterValue() as string) ?? ""
-            }
-            onChange={(e) =>
-              table.getColumn("loteFabrico")?.setFilterValue(e.target.value)
-            }
-            className="max-w-sm"
-          />
+        <div className="flex flex-row items-center py-4 gap-x-2 ">
+          <div className="flex flex-row gap-x-2">
+            <Input
+              placeholder="Filtrar Status ... "
+              value={
+                (table.getColumn("estadoNovo")?.getFilterValue() as string) ??
+                ""
+              }
+              onChange={(e) =>
+                table.getColumn("estadoNovo")?.setFilterValue(e.target.value)
+              }
+              className="max-w-sm"
+            />
+            <Input
+              placeholder="Filtrar Lote ... "
+              value={
+                (table.getColumn("loteFabrico")?.getFilterValue() as string) ??
+                ""
+              }
+              onChange={(e) =>
+                table.getColumn("loteFabrico")?.setFilterValue(e.target.value)
+              }
+              className="max-w-sm"
+            />
+          </div>
         </div>
-        <div className="flex mr-3"></div>
+        <div className="flex mr-3">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                id="date"
+                variant={"outline"}
+                className={cn(
+                  "w-[300px] justify-start text-left font-normal",
+                  !date && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {date?.from ? (
+                  date.to ? (
+                    <>
+                      {format(date.from, "LLL dd, y")} -{" "}
+                      {format(date.to, "LLL dd, y")}
+                    </>
+                  ) : (
+                    format(date.from, "LLL dd, y")
+                  )
+                ) : (
+                  <span>Filtrar por Data</span>
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                locale={pt}
+                initialFocus
+                mode="range"
+                defaultMonth={date?.from}
+                selected={date}
+                onSelect={setDate}
+                numberOfMonths={2}
+              />
+            </PopoverContent>
+          </Popover>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  className="ml-2"
+                  size={"icon"}
+                  onClick={() => {
+                    //console.log(table.getColumn("timestamp")?.getFilterValue());
+                    //console.log(table.getColumn("timestamp")?.getFilterFn());
+                    //console.log(table.getColumn("timestamp")?.getFilterValue());
+                    //console.log(table.getColumn("timestamp"));
+                    table.getColumn("timestamp")?.setFilterValue(date);
+                  }}
+                >
+                  <CalendarSearch />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Filtrar por Data</p>
+                <p>Predefinido: ultimos 30 dias</p>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  className="ml-2"
+                  size={"icon"}
+                  onClick={() =>
+                    setDate({
+                      from: subDays(new Date(), 30),
+                      to: new Date(),
+                    })
+                  }
+                >
+                  <CalendarMinus2 />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Definir para Predefinição</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
       </div>
       <div className="rounded-md border-2 border-lime-500">
         <Table>
@@ -131,53 +239,3 @@ export default function DataTable<TData, TValue>({
     </div>
   );
 }
-/*
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination"
- 
-export function PaginationDemo() {
-  return (
-    <Pagination>
-      <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious href="#" />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#" isActive>
-            2
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">3</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext href="#" />
-        </PaginationItem>
-      </PaginationContent>
-    </Pagination>
-  )
-}
-
-
-NEXT 
-  <PaginationItem>
--   <a>
-+   <Link>
-      // ...
--   </a>
-+   </Link>
-  </PaginationItem>
-*/
