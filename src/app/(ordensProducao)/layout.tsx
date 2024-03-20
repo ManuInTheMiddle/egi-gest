3aa643c84be35d9eaac9e8d2f2b34b7a465f4d79
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2024-03-20 11:29:56 +0000

    added tanstack provider to ordens producao

--- src/app/(ordensProducao)/layout.tsx
+++ src/app/(ordensProducao)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import StoreProvider from "../StoreProvider";
-
+import TanstackProvider from "../../../providers/TanstackProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,7 +19,9 @@ export default function RootLayout({
     <html lang="en" className="no-scrollbar">
       <body className={inter.className}>
         <section>
-          <StoreProvider>{children}</StoreProvider>
+          <TanstackProvider>
+            <StoreProvider>{children}</StoreProvider>
+          </TanstackProvider>
         </section>
       </body>
     </html>
